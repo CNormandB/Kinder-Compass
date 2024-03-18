d7d3dcf66deabf6213c8f251cf127f1d1d2751db
Casandra Normand <casandra.normand@outlook.com>
2024-03-18 12:24:53 -0400

    fix: spacing fixed in file to be easier to read

--- src/components/ELECTSelection.jsx
+++ src/components/ELECTSelection.jsx
@@ -1,10 +1,10 @@
 function ELECTSelection(){
   return(
 <select className="aspects_of_development"> 
-<option>ELECT Aspects</option>
-<option value="infant">Infant</option> 
-<option value="toddler">Toddler</option>
-<option value="preschool">Preschool</option>  
+  <option>ELECT Aspects</option>
+  <option value="infant">Infant</option> 
+  <option value="toddler">Toddler</option>
+  <option value="preschool">Preschool</option>  
 </select>
 );
 }
