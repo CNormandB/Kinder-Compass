83e9d5431666cfc0619c5b9b5ce49077d1c18ed5
Casandra Normand <casandra.normand@outlook.com>
2024-03-18 12:23:26 -0400

    feature: Interest Selection placeholder dropdown

---
+++ src/components/InterestSelection.jsx
@@ -0,0 +1,12 @@
+function InterestSelection(){
+  return(
+  <select className="interest"> 
+    <option>Interests</option>
+    <option value="dinosaur">Dinosaur</option> 
+    <option value="transportation">Transportation</option>
+    <option value="other">Other</option>  
+</select>
+  );
+}
+
+export default InterestSelection;
