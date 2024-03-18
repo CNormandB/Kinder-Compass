b2ba3410c80b23fab278aab650f50197f10ad2da
Casandra Normand <casandra.normand@outlook.com>
2024-03-18 12:19:00 -0400

    feature: placeholder for ELECT options

---
+++ src/components/ELECTSelection.jsx
@@ -0,0 +1,12 @@
+function ELECTSelection(){
+  return(
+<select className="aspects_of_development"> 
+<option>ELECT Aspects</option>
+<option value="infant">Infant</option> 
+<option value="toddler">Toddler</option>
+<option value="preschool">Preschool</option>  
+</select>
+);
+}
+
+export default ELECTSelection;
