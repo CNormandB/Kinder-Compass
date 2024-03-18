e9414aaae591fe6370c9bda104f1a7e36228836c
Casandra Normand <casandra.normand@outlook.com>
2024-03-18 14:50:45 -0400

    created a base for the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+function Header(){
+return(
+  <section className="header_container">
+    <div className="header_logo">
+      <img src="Logo.png"/>
+      <h1>Kinder Compass</h1>
+    </div>
+  </section>
+)
+}
+
+export default Header;
